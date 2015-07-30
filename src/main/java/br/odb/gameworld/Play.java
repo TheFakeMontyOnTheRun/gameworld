2c59af2b0486e3245055b498d0dec61eacf1d21a
Daniel Monteiro <danielmonteiro@id.uff.br>
2015-07-30 00:10:08 -0300

    Ading a central piece to unify the sense of the story
    A unifying idea every game could extend. Sounds nice.

---
+++ src/main/java/br/odb/gameworld/Play.java
@@ -0,0 +1,10 @@
+package br.odb.gameworld;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Play {
+
+	public final List<CharacterActor> actors = new ArrayList<>();
+	Location stage;
+}
